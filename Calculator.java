21c08cb754624e17798028d6ec0e4452ebc83d24
Studio528-real <52116771+Studio528-real@users.noreply.github.com>
2020-02-09 00:19:56 -0700

    Update Calculator.java
    Made it more comapct

--- Calculator.java
+++ Calculator.java
@@ -8,54 +8,55 @@ public class Calculator {
         Scanner input = new Scanner(System.in);
         int operation, x, y;
         operation = input.nextInt();
-        if (operation == 0) {
-            do {
-                System.out.println("stopping");
+        switch (operation) {
+            case 0:
+                System.out.println("Confirm(0), Cancel(1)");
+                x = input.nextInt();
+                if (x == 0)
+                    break;
+                else
+                    System.out.println("idk how to make this work yet, closing");
+                    break;
+            case 1:
+                System.out.println("Choose a number");
+                x = input.nextInt();
+                System.out.println("Choose another number");
+                y = input.nextInt();
+                System.out.println(x + y);
                 break;
-            } while (true);
+            case 2:
+                System.out.println("Choose a number");
+                x = input.nextInt();
+                System.out.println("Choose another number");
+                y = input.nextInt();
+                System.out.println(x - y);
+                break;
+            case 3:
+                System.out.println("Choose a number");
+                x = input.nextInt();
+                System.out.println("Choose another number");
+                y = input.nextInt();
+                System.out.println(x * y);
+                break;
+            case 4:
+                System.out.println("Choose a number");
+                x = input.nextInt();
+                System.out.println("Choose another number");
+                y = input.nextInt();
+                System.out.println(x / y);
+                break;
+            case 5:
+                System.out.println("Choose a number");
+                x = input.nextInt();
+                System.out.println(x * x);
+                break;
+            case 6:
+                System.out.println("Choose a number");
+                x = input.nextInt();
+                System.out.println(sqrt(x));
+                break;
+            default:
+                System.out.println("Not a Valid Option");
         }
-        if (operation == 1) {
-            System.out.println("Choose a number");
-            x = input.nextInt();
-            System.out.println("Choose another number");
-            y = input.nextInt();
-            System.out.println(x + y);
-        }
-        if (operation == 2) {
-            System.out.println("Choose a number");
-            x = input.nextInt();
-            System.out.println("Choose another number");
-            y = input.nextInt();
-            System.out.println(x - y);
-        }
-        if (operation == 3) {
-            System.out.println("Choose a number");
-            x = input.nextInt();
-            System.out.println("Choose another number");
-            y = input.nextInt();
-            System.out.println(x * y);
-        }
-        if (operation == 4) {
-            System.out.println("Choose a number");
-            x = input.nextInt();
-            System.out.println("Choose another number");
-            y = input.nextInt();
-            System.out.println(x / y);
-        }
-        if (operation == 5) {
-            System.out.println("Choose a number");
-            x = input.nextInt();
-            System.out.println(x * x);
-        }
-        if (operation == 6) {
-            System.out.println("Choose a number");
-            x = input.nextInt();
-            System.out.println(sqrt(x));
-        }
-
-
-
-
-
     }
 }
