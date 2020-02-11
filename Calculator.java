530d3504b1bc514d5a840eb274267c85c46778fc
Studio528-real <52116771+Studio528-real@users.noreply.github.com>
2020-02-11 12:52:52 -0700

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -3,10 +3,60 @@ import java.util.Scanner;
 import static java.lang.Math.sqrt;
 
 public class Calculator {
+    static void Add(){
+        Scanner input = new Scanner(System.in);
+        int x, y;
+        System.out.println("Choose a number");
+        x = input.nextInt();
+        System.out.println("Choose another number");
+        y = input.nextInt();
+        System.out.println(x + y);
+    }
+    static void Sub(){
+        Scanner input = new Scanner(System.in);
+        int x, y;
+        System.out.println("Choose a number");
+        x = input.nextInt();
+        System.out.println("Choose another number");
+        y = input.nextInt();
+        System.out.println(x - y);
+    }
+    static void Mul() {
+        Scanner input = new Scanner(System.in);
+        int x, y;
+        System.out.println("Choose a number");
+        x = input.nextInt();
+        System.out.println("Choose another number");
+        y = input.nextInt();
+        System.out.println(x * y);
+    }
+    static void div(){
+        Scanner input = new Scanner(System.in);
+        int x, y;
+        System.out.println("Choose a number");
+        x = input.nextInt();
+        System.out.println("Choose another number");
+        y = input.nextInt();
+        System.out.println(x / y);
+    }
+    static void sqr(){
+        Scanner input = new Scanner(System.in);
+        int x;
+        System.out.println("Choose a number");
+        x = input.nextInt();
+        System.out.println(x * x);
+    }
+    static void sqroot(){
+        Scanner input = new Scanner(System.in);
+        int x;
+        System.out.println("Choose a number");
+        x = input.nextInt();
+        System.out.println(sqrt(x));
+    }
     public static void main(String[] args) {
         System.out.println("add(1), subtract(2), multiply(3), divide(4), square(5), Square root(6), quit(0): ");
         Scanner input = new Scanner(System.in);
-        int operation, x, y;
+        int operation, x;
         operation = input.nextInt();
         switch (operation) {
             case 0:
@@ -18,42 +68,22 @@ public class Calculator {
                     System.out.println("idk how to make this work yet, closing");
                     break;
             case 1:
-                System.out.println("Choose a number");
-                x = input.nextInt();
-                System.out.println("Choose another number");
-                y = input.nextInt();
-                System.out.println(x + y);
+                Add();
                 break;
             case 2:
-                System.out.println("Choose a number");
-                x = input.nextInt();
-                System.out.println("Choose another number");
-                y = input.nextInt();
-                System.out.println(x - y);
+                Sub();
                 break;
             case 3:
-                System.out.println("Choose a number");
-                x = input.nextInt();
-                System.out.println("Choose another number");
-                y = input.nextInt();
-                System.out.println(x * y);
+                Mul();
                 break;
             case 4:
-                System.out.println("Choose a number");
-                x = input.nextInt();
-                System.out.println("Choose another number");
-                y = input.nextInt();
-                System.out.println(x / y);
+                div();
                 break;
             case 5:
-                System.out.println("Choose a number");
-                x = input.nextInt();
-                System.out.println(x * x);
+                sqr();
                 break;
             case 6:
-                System.out.println("Choose a number");
-                x = input.nextInt();
-                System.out.println(sqrt(x));
+                sqroot();
                 break;
             default:
                 System.out.println("Not a Valid Option");
