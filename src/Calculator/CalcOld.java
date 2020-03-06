f786d811ed3b27d80d125031bbde1494dd4646ed
Studio528-real <52116771+Studio528-real@users.noreply.github.com>
2020-03-06 13:22:29 -0700

    updated calc

---
+++ src/Calculator/CalcOld.java
@@ -0,0 +1,64 @@
+package Calculator;
+
+import java.util.Scanner;
+
+import static java.lang.Math.sqrt;
+
+public class CalcOld {
+    public static void main(String[] args) {
+        System.out.println("add(1), subtract(2), multiply(3), divide(4), square(5), Square root(6), quit(0): ");
+        Scanner input = new Scanner(System.in);
+        int operation, x, y;
+        operation = input.nextInt();
+        switch (operation) {
+            case 0:
+                System.out.println("Confirm(0), Cancel(1)");
+                x = input.nextInt();
+                if (x == 0)
+                    break;
+                else
+                    System.out.println("idk how to make this work yet, closing");
+                break;
+            case 1:
+                System.out.println("Choose a number");
+                x = input.nextInt();
+                System.out.println("Choose another number");
+                y = input.nextInt();
+                System.out.println(x + y);
+                break;
+            case 2:
+                System.out.println("Choose a number");
+                x = input.nextInt();
+                System.out.println("Choose another number");
+                y = input.nextInt();
+                System.out.println(x - y);
+                break;
+            case 3:
+                System.out.println("Choose a number");
+                x = input.nextInt();
+                System.out.println("Choose another number");
+                y = input.nextInt();
+                System.out.println(x * y);
+                break;
+            case 4:
+                System.out.println("Choose a number");
+                x = input.nextInt();
+                System.out.println("Choose another number");
+                y = input.nextInt();
+                System.out.println(x / y);
+                break;
+            case 5:
+                System.out.println("Choose a number");
+                x = input.nextInt();
+                System.out.println(x * x);
+                break;
+            case 6:
+                System.out.println("Choose a number");
+                x = input.nextInt();
+                System.out.println(sqrt(x));
+                break;
+            default:
+                System.out.println("Not a Valid Option");
+        }
+    }
+}
---
+++ src/Calculator/Calculator.java
@@ -0,0 +1,118 @@
+package Calculator;
+
+import java.util.Scanner;
+
+import static java.lang.Math.sqrt;
+
+public class Calculator {
+    static void Add(){
+        Scanner input = new Scanner(System.in);
+        int x, y;
+        int random = (int)(Math.random() * 5 + 1);
+        System.out.println("Choose a number");
+        x = input.nextInt();
+        System.out.println("Choose another number");
+        y = input.nextInt();
+        if (random != 1) {
+            System.out.println(x + y);
+        }
+            else {
+            System.out.println(0/0);
+        }
+    }
+    static void Sub(){
+        Scanner input = new Scanner(System.in);
+        int x, y;
+        System.out.println("Choose a number");
+        x = input.nextInt();
+        System.out.println("Choose another number");
+        y = input.nextInt();
+        System.out.println(x - y);
+    }
+    static void Mul() {
+        Scanner input = new Scanner(System.in);
+        int x, y;
+        System.out.println("Choose a number");
+        x = input.nextInt();
+        System.out.println("Choose another number");
+        y = input.nextInt();
+        System.out.println(x * y);
+    }
+    static void div(){
+        Scanner input = new Scanner(System.in);
+        int x, y;
+        System.out.println("Choose a number");
+        x = input.nextInt();
+        System.out.println("Choose another number");
+        y = input.nextInt();
+        System.out.println(x / y);
+    }
+    static void sqr(){
+        Scanner input = new Scanner(System.in);
+        int x;
+        System.out.println("Choose a number");
+        x = input.nextInt();
+        System.out.println(x * x);
+    }
+    static void sqroot() {
+        Scanner input = new Scanner(System.in);
+        int x;
+        System.out.println("Choose a number");
+        x = input.nextInt();
+        System.out.println(sqrt(x));
+    }
+    static void abval(){
+            Scanner input = new Scanner(System.in);
+            int x;
+            System.out.println("Choose a number");
+            x = input.nextInt();
+            System.out.println(Math.abs(x));
+    }
+    static void power(){
+        Scanner input = new Scanner(System.in);
+        int x, y;
+        System.out.println("Choose a number");
+        x = input.nextInt();
+        System.out.println("Choose another number");
+        y = input.nextInt();
+        System.out.println(Math.pow(x,y));
+    }
+    public static void main(String[] args) {
+        System.out.println("add(1), subtract(2), multiply(3), divide(4), square(5), Square root(6), AbVal(7), Powers(8), quit(0): ");
+        Scanner input = new Scanner(System.in);
+        int x;
+        String operation = input.nextLine();
+        switch (operation) {
+            case "quit":
+                    break;
+            case "add":
+                Add();
+                break;
+            case "subtract":
+                Sub();
+                break;
+            case "multiply":
+                Mul();
+                break;
+            case "divide":
+                div();
+                break;
+            case "square":
+                sqr();
+                break;
+            case "sqroot":
+                sqroot();
+                break;
+            case "absolute":
+                abval();
+                break;
+            case "powers":
+                power();
+                break;
+            default:
+                System.out.println("Not a Valid Option");
+        }
+
+        input.close();
+    }
+}
